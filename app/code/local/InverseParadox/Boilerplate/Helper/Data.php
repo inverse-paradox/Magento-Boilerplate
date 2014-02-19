f5df5e1c1f55a3d4fba5f6c257961332da3b9a1f
Amanda Mitchell <amanda@inverseparadox.com>
2014-02-19 15:25:03 -0500

    Move column count class generation to helper

--- app/code/local/InverseParadox/Boilerplate/Helper/Data.php
+++ app/code/local/InverseParadox/Boilerplate/Helper/Data.php
@@ -1,5 +1,42 @@
 <?php
+/**
+ * Boilerplate general helper
+ *
+ * @package     InverseParadox_Boilerplate
+ */
 class InverseParadox_Boilerplate_Helper_Data extends Mage_Core_Helper_Abstract
 {
     
+    /**
+     * Generate classes for responsive columns
+     *
+     * @param int $count index of item to generate class for
+     * @return string class(es) to apply to item
+     */
+    function getColumnClasses($count)
+    {
+        $itemclass = '';
+        if ($count % 2 == 0) {
+            $itemclass = 'even';
+        } else {
+            $itemclass = 'odd';
+        }
+        if ($count % 3 == 0) {
+            $itemclass .= ' third';
+        } else if (($count - 1) % 3 == 0) {
+            $itemclass .= ' after-third';
+        }
+        if ($count % 4 == 0) {
+            $itemclass .= ' fourth';
+        } else if (($count - 1) % 4 == 0) {
+            $itemclass .= ' after-fourth';
+        }
+        if ($count % 5 == 0) {
+            $itemclass .= ' fifth';
+        } else if (($count - 1) % 5 == 0) {
+            $itemclass .= ' after-fifth';
+        }
+        return $itemclass;
+    }
+
 }
